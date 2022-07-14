871df2894d98e4021b156da6a6cd5d8373955ccf
boioib <109253331+boioib@users.noreply.github.com>
2022-07-14 13:05:16 -0700

    simple_adder.py

--- _04_int/_2_simple_number_adder/simple_adder.py
+++ _04_int/_2_simple_number_adder/simple_adder.py
@@ -3,3 +3,13 @@
 
 * Display the sum of the two numbers to the user
 """
+from tkinter import messagebox, simpledialog, Tk
+import tkinter as tk
+window = Tk()
+window.withdraw()
+
+number_1=simpledialog.askinteger(title='Task',prompt='Enter a number')
+number_2=simpledialog.askinteger(title='Task',prompt='Enter another number')
+sum=number_1+number_2
+print(sum)
+messagebox.showinfo(title='Sum',message=sum)
